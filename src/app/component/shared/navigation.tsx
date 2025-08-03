311881140c379431fa53591f56870ea0a2d5452d
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-08-03 00:04:40 -0700

    nav update

--- src/app/component/shared/navigation.tsx
+++ src/app/component/shared/navigation.tsx
@@ -22,13 +22,13 @@ export default function Navigation() {
     { href: '/post', icon: PlusCircle, filledIcon: PlusCircleFill },
     { href: '/notification', icon: Bell, filledIcon: BellFill },
     { href: '/setting', icon: Gear, filledIcon: GearFill },
-    { href: '/login', icon: Google, filledIcon: Google },
+    //{ href: '/login', icon: Google, filledIcon: Google },
   ];
 
     return (
     <div className='flex md:inline-flex flex-row md:flex-col sticky bottom-0 left-0 md:h-svh md:justify-center'>
       <LogoDesktop />
-      <ul className='flex flex-row md:flex-col justify-center w-full gap-4 md:gap-8 px-11 py-3 pb-7 md:py-11 md:px-5 bg-background/70 backdrop-blur-sm md:bg-transparent md:backdrop-blur-none'>
+      <ul className='flex flex-row md:flex-col justify-center w-full gap-4 md:gap-8 px-11 py-6 md:py-11 md:px-5 bg-background/70 backdrop-blur-sm md:bg-transparent md:backdrop-blur-none'>
         {navItems.map(({ href, icon: Icon, filledIcon: FilledIcon }) => {
           const isActive = pathname === href;
           const IconComponent = isActive ? FilledIcon : Icon;
