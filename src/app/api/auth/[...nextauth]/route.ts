bc6ea60ea939b76879091b5c7f7e757fad1f9cd9
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-08-14 12:08:46 -0700

    auth/nextauth modified

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+// @ts-expect-error: type error
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
