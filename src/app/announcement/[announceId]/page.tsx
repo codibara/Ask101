244cac9d7b1cf9a6d3c1117f597cc24384f15e19
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-08-14 11:38:04 -0700

    announcement error resolved

--- src/app/announcement/[announceId]/page.tsx
+++ src/app/announcement/[announceId]/page.tsx
@@ -4,16 +4,16 @@ import { useEffect, useState } from 'react';
 
 import { Announcement } from '@/types/announcement';
 import { getAnnounceById } from "@/lib/announceService";
-
 import PageHeader from '@/app/component/shared/pageHeader';
 
-interface Props {
+export default function PostDetail({
+  params,
+}: {
   params: { announceId: string };
-}
-
-export default function PostDetail({ params }: Props) {
+}) {
   const [announce, setAnnounce] = useState<Announcement | null>(null);
 
+  // Convert URL param from string → number
   const announceId = parseInt(params.announceId, 10);
 
   useEffect(() => {
@@ -24,24 +24,18 @@ export default function PostDetail({ params }: Props) {
     return <div className="p-10 text-red-500">Post not found</div>;
   }
 
-  const {
-    title,
-    content,
-    date,
-  } = announce;
+  const { title, content, date } = announce;
 
   return (
     <main className="min-h-[calc(100svh-160px)] px-5 pb-[88px] md:px-26 md:py-5">
-      <div className='max-w-2xl mx-auto'>
-      <PageHeader title='공지' showDropdown={false}/>
-        {/* Post Details */}
+      <div className="max-w-2xl mx-auto">
+        <PageHeader title="공지" showDropdown={false} />
         <div className="flex flex-col items-start gap-4 my-5">
           <div className="w-full">
             <h1 className="text-2xl font-bold mb-1.5">{title}</h1>
             <p className="text-sm">{date}</p>
           </div>
           <p className="text-base">{content}</p>
-          
         </div>
       </div>
     </main>
